aa456884e314990cc008a82f7d886a9873caabb0
varun040503 <varunreddy2439@gmail.com>
2023-06-22 00:54:02 +0530

    Updates

---
+++ precache-manifest.3912c854622787b7ce334d3cacb89701.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2a7dc7391361297c649",
+    "url": "/agecalculator/static/css/main.3ff7bdfc.chunk.css"
+  },
+  {
+    "revision": "a2a7dc7391361297c649",
+    "url": "/agecalculator/static/js/main.a2a7dc73.chunk.js"
+  },
+  {
+    "revision": "3f5be87d2a8834347623",
+    "url": "/agecalculator/static/js/1.3f5be87d.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/agecalculator/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "eac28f7789be2ee12a1a",
+    "url": "/agecalculator/static/js/runtime~main.eac28f77.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/agecalculator/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "056972c1635be6b45cd520aec0faff91",
+    "url": "/agecalculator/index.html"
+  }
+];
